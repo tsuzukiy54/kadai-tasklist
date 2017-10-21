adf652766a66df04cf5962146c8f3793016913de
YujiTsuzuki <yuji.tsuzuki@g.softbank.co.jp>
2017-10-21 08:51:02 +0000

    login-2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:show, :new, :create, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show :edit, :update, :destroy]
+
 
   def index
     if logged_in?
@@ -58,6 +60,13 @@ class TasksController < ApplicationController
     redirect_to tasks_url
   end
 
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 
 private
 
